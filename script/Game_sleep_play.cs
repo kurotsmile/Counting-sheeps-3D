d56d274822fc384219d6572a1ccc270c6d53adae
Trần Thiện Thanh <kurotsmile@gmail.com>
2023-08-21 13:22:33 +0700

    Fix sheep play
    Fix sheep play

--- script/Game_sleep_play.cs
+++ script/Game_sleep_play.cs
@@ -23,22 +23,10 @@ public class Game_sleep_play : MonoBehaviour {
 	[Header("Obj Game")]
 	public game_handle games;
 	public GameObject[] sheep;
-	public GameObject panel_good_night;
-	public GameObject panel_write_good_night;
-	public GameObject panel_account_login;
 
 	public Text txt_good_night_msg;
 	public Text txt_good_night_name;
 	public Text txt_good_night_date;
-
-	[Header("Account current")]
-	public Text txt_good_night_account_login_username;
-	public Image img_goood_night_account_login_avt;
-	public Image img_good_night_avatar;
-	private string id_user_good_night_view;
-
-	public InputField inp_gn_name;
-	public InputField inp_gn_msg;
 
 	public Transform point_start;
 	public float time_create = 0f;
@@ -49,15 +37,11 @@ public class Game_sleep_play : MonoBehaviour {
 
 	public Image img_btn_pause;
 	public Image img_btn_pause_music;
-	public Image img_btn_msg;
 	public Sprite icon_play;
 	public Sprite icon_pause;
-	public Sprite icon_msg_yes;
-	public Sprite icon_msg_no;
 	public Sprite icon_good_night_avt_default;
 
 	private bool is_run = true;
-	private bool is_show_msg=true;
 
 	void Start () {
 		this.create_sheep ();
@@ -94,8 +78,6 @@ public class Game_sleep_play : MonoBehaviour {
 		this.img_btn_pause.sprite = this.icon_pause;
 		this.img_btn_pause_music.sprite = this.icon_play;
 		this.update_name_music ();
-		this.panel_good_night.SetActive (false);
-		this.panel_write_good_night.SetActive (false);
 	}
 
 	public void update_name_music(){
@@ -150,113 +132,4 @@ public class Game_sleep_play : MonoBehaviour {
 			}
 		}
 	}
-
-	public void good_night(){
-		if (this.is_show_msg) {
-			this.StopAllCoroutines ();
-			if (this.panel_good_night.activeInHierarchy) {
-				this.panel_good_night.SetActive (false);
-			} else {
-				this.img_good_night_avatar.sprite = this.icon_good_night_avt_default;
-
-			}
-		}
-	}
-	private void get_good_night(string s_data)
-	{
-		IDictionary data = (IDictionary)Carrot.Json.Deserialize(s_data);
-		this.txt_good_night_msg.text = data["msg"].ToString();
-		this.txt_good_night_name.text = data["name_user"].ToString();
-		this.txt_good_night_date.text = data["date"].ToString();
-		this.id_user_good_night_view = data["id_user"].ToString();
-		if (data["avatar_user"].ToString() != "") this.games.carrot.get_img(data["avatar_user"].ToString(), this.img_good_night_avatar);
-		this.panel_good_night.SetActive(true);
-	}
-
-	public void submit_good_night(){
-		if (this.panel_account_login.activeInHierarchy == false) {
-			if (this.inp_gn_name.text == null || this.inp_gn_name.text.Length <= 5) {
-				this.games.carrot.show_msg(PlayerPrefs.GetString("good_night_write", "Write Good Night"), PlayerPrefs.GetString("error_name", "Your name must not be longer than 5 characters"), Carrot.Msg_Icon.Error);
-				return;
-			}
-		}
-
-		if (this.inp_gn_msg.text == null || this.inp_gn_msg.text.Length <= 10) {
-			this.games.carrot.show_msg(PlayerPrefs.GetString("good_night_write", "Write Good Night"), PlayerPrefs.GetString("error_msg", "The message can not be blank and is greater than 5 characters"),Carrot.Msg_Icon.Error);
-			return;
-		}
-		WWWForm frm_send_goodnight = this.games.carrot.frm_act("send_good_night");
-		frm_send_goodnight.AddField("msg", this.inp_gn_msg.text);
-		if (this.games.carrot.user.get_id_user_login()!="")
-		{
-			frm_send_goodnight.AddField("user_name", this.games.carrot.user.get_id_user_login());
-			frm_send_goodnight.AddField("user_type", "1");
-		}
-		else
-		{
-			frm_send_goodnight.AddField("user_name", this.inp_gn_name.text);
-			frm_send_goodnight.AddField("user_type", "0");
-		}
-
-		Good_Night_Data good_night = new Good_Night_Data();
-		good_night.msg = this.inp_gn_msg.text;
-
-
-        if (this.games.carrot.user.get_id_user_login() != "")
-        {
-			Carrot.Carrot_Rate_user_data user_login = new Carrot_Rate_user_data();
-			user_login.name = this.games.carrot.user.get_data_user_login("name");
-			user_login.id = this.games.carrot.user.get_id_user_login();
-			user_login.lang = this.games.carrot.user.get_lang_user_login();
-			user_login.avatar = this.games.carrot.user.get_data_user_login("avatar");
-			good_night.user = user_login;
-		}
-
-		this.games.carrot.db.Collection("good_night").AddAsync(good_night);
-
-		this.games.carrot.show_msg(PlayerPrefs.GetString("good_night_write", "Write Good Night"), PlayerPrefs.GetString("good_night_success", "Good night success!!!"), Carrot.Msg_Icon.Success);
-		this.btn_close_write_good_night();
-	}
-
-	public void btn_show_msg(){
-		this.games.carrot.play_sound_click();
-		if (this.is_show_msg) {
-			this.StopAllCoroutines ();
-			this.img_btn_msg.sprite = icon_msg_yes;
-			this.panel_good_night.SetActive (false);
-			this.is_show_msg = false;
-		} else {
-			this.img_btn_msg.sprite = icon_msg_no;
-			this.is_show_msg = true;
-		}
-	}
-
-	public void btn_show_write_good_night(){
-		this.games.carrot.play_sound_click();
-        this.btn_pause();
-		this.panel_write_good_night.SetActive (true);
-		string id_user_login =this.games.carrot.user.get_id_user_login();
-		if (id_user_login != "") {
-			this.panel_account_login.SetActive (true);
-			this.txt_good_night_account_login_username.text =this.games.carrot.user.get_data_user_login("name");
-		} else{
-			this.panel_account_login.SetActive (false);
-		}
-	}
-
-    public void btn_close_write_good_night()
-    {
-		this.games.carrot.play_sound_click();
-        this.panel_write_good_night.SetActive(false);
-        this.game_unPause();
-    }
-
-	public void btn_view_user_good_night(){
-		if (this.id_user_good_night_view != "") {
-			this.games.carrot.play_sound_click();
-			string lang_user_login = this.games.carrot.user.get_lang_user_login();
-			this.games.carrot.user.show_user_by_id(this.id_user_good_night_view, lang_user_login);
-		}
-	}
-
 }
--- script/Sheep.cs
+++ script/Sheep.cs
@@ -1,98 +1,103 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Sheep : MonoBehaviour {
-	private float speed=1f;
+public class Sheep : MonoBehaviour
+{
+	private float speed = 1f;
 	public Animator ani;
 	public Rigidbody rig;
-	private bool is_run=true;
+	private bool is_run = true;
 	private bool is_jump_game = false;
-	public int id=-1;
-	// Use this for initialization
-	void Start () {
-		ani.Play ("run");
+	public int id = -1;
+
+	void Start()
+	{
+		ani.Play("run");
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (this.is_run) {
+
+	void Update()
+	{
+		if (this.is_run)
+		{
 			this.speed = 1f * Time.deltaTime;
-			transform.position += new Vector3 (0, 0, this.speed);
+			transform.position += new Vector3(0, 0, this.speed);
 		}
 	}
 
-	public void jump(){
-		ani.Play ("Atk");
-		this.rig.AddForce (0, 300f, 110f);
-		GameObject.Find("Game_sleep_play").GetComponent<Game_sleep_play> ().add_sheep ();
+	public void jump()
+	{
+		ani.Play("Atk");
+		this.rig.AddForce(0, 300f, 110f);
+		GameObject.Find("Game_sleep_play").GetComponent<Game_sleep_play>().add_sheep();
 	}
 
-	public void jump_game(){
-		if (this.is_jump_game) {
-			ani.Play ("Atk");
-			this.rig.AddForce (0,  Random.Range(230f,301f), Random.Range(60f,111f));
+	public void jump_game()
+	{
+		if (this.is_jump_game)
+		{
+			ani.Play("Atk");
+			this.rig.AddForce(0, Random.Range(230f, 301f), Random.Range(60f, 111f));
 			this.is_jump_game = false;
 		}
 	}
 
-
 	void OnTriggerEnter(Collider col)
-    {
-		if (this.is_run) {
-			if (col.gameObject.name == "jump") {
-				this.jump ();
+	{
+		if (this.is_run)
+		{
+			if (col.gameObject.name == "jump")
+			{
+				this.jump();
 			}
 
-			if (col.gameObject.name == "add") {
+			if (col.gameObject.name == "add")
+			{
 				this.id = -1;
-				GameObject.Find ("Game_sheep_play").GetComponent<Game_sheep_play> ().add_sheep ();
+				GameObject.Find("Game_sheep_play").GetComponent<Game_sheep_play>().add_sheep();
 			}
 
-			if (col.gameObject.name == "show_tag") {
-				GameObject.Find ("Game_sheep_play").GetComponent<Game_sheep_play> ().show_tag_act (this.id);
+			if (col.gameObject.name == "show_tag")
+			{
+				GameObject.Find("Game_sheep_play").GetComponent<Game_sheep_play>().show_tag_act(this.id);
 				this.is_jump_game = true;
 			}
 
-			if (col.gameObject.name == "die") {
-				ani.Play ("Die");
-				this.rig.AddForce (0, 50f, -50f);
+			if (col.gameObject.name == "die")
+			{
+				ani.Play("Die");
+				this.rig.AddForce(0, 50f, -50f);
 				this.is_run = false;
-				Destroy (this.GetComponent<BoxCollider> ());
-				Destroy (this.GetComponent<Rigidbody> ());
-				GameObject.Find ("Game_sheep_play").GetComponent<Game_sheep_play> ().hide_tag_act (this.id);
-				if (PlayerPrefs.GetInt ("is_sound_mute", 0) == 0) {
-                    GameObject.Find("game_handle").GetComponent<game_handle>().play_sound(2);
-                }
-				GameObject.Find ("Game_sheep_play").GetComponent<Game_sheep_play> ().remove_heart ();
-				Destroy (this.gameObject, 0.8f);
-			}
-
-			if (col.gameObject.name == "show_good_night") {
-				GameObject.Find ("Game_sleep_play").GetComponent<Game_sleep_play> ().good_night ();
+				Destroy(this.GetComponent<BoxCollider>());
+				Destroy(this.GetComponent<Rigidbody>());
+				GameObject.Find("Game_sheep_play").GetComponent<Game_sheep_play>().hide_tag_act(this.id);
+				if (PlayerPrefs.GetInt("is_sound_mute", 0) == 0)
+				{
+					GameObject.Find("game_handle").GetComponent<game_handle>().play_sound(2);
+				}
+				GameObject.Find("Game_sheep_play").GetComponent<Game_sheep_play>().remove_heart();
+				Destroy(this.gameObject, 0.8f);
 			}
 		}
-    }
+	}
 
+	public void pause()
+	{
+		ani.Play("Idle");
+		this.is_run = false;
+	}
 
+	public void unPause()
+	{
+		ani.Play("run");
+		this.is_run = true;
+	}
 
-    public void pause()
-    {
-        ani.Play("Idle");
-        this.is_run = false;
-    }
-
-    public void unPause()
-    {
-        ani.Play("run");
-        this.is_run = true;
-    }
-
-	void OnMouseDown(){
-		if (this.id != -1) {
-			this.jump_game ();
-            GameObject.Find("game_handle").GetComponent<game_handle>().play_sound(1);
-            GameObject.Find ("Game_sheep_play").GetComponent<Game_sheep_play> ().hide_tag_act (this.id);
+	void OnMouseDown()
+	{
+		if (this.id != -1)
+		{
+			this.jump_game();
+			GameObject.Find("game_handle").GetComponent<game_handle>().play_sound(1);
+			GameObject.Find("Game_sheep_play").GetComponent<Game_sheep_play>().hide_tag_act(this.id);
 		}
 	}
 }
